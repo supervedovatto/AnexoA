a3819ab4571363beac20cc007fd257165430e4fc
supervedovatto <supervedovatto@hotmail.com>
2020-05-03 18:07:25 -0300

    Tabela apresentando a população total em cada das cidades que compõem a microrregião de referência

---
+++ TabelaPopulacaoMicro.R
@@ -0,0 +1,13 @@
+AreaMicro <- Area %>% 
+  filter(Ano == max(Area$Ano) & Mesorregiao == LocRef$Mesorregiao) %>% 
+  group_by(Microrregiao) %>% 
+  summarise(AreaTerritorial = sum(AreaTerritorial))
+
+PopulacaoProjecao %>% 
+  filter(Mesorregiao == LocRef$Mesorregiao,Ano == AnoRef) %>% 
+  group_by(Microrregiao) %>% 
+  summarise(Populacao = sum(Quantidade)) %>% 
+  merge(AreaMicro,by = "Microrregiao") %>% 
+  mutate(Densidade = round(Populacao/AreaTerritorial,digits = 2)) %>% 
+  kable(col.names = c("Microrregião","População (hab)","Área (km²)","Densidade Populacional (hab/km²)"),caption = paste("População, área e densidade populacional da Mesorregião",LocRef$Mesorregiao))
+rm(AreaMicro)
