3db75ea21fa414015f7df1b88110a5775dadcc0b
supervedovatto <supervedovatto@hotmail.com>
2020-05-03 20:42:07 -0300

    Dados básicos sobre a região de Planejamento

---
+++ RegiaoPlanejamento.R
@@ -0,0 +1,19 @@
+load(file = "Dados/POCV.RData")
+
+LocalSEGPLAN <- RegioesGoias %>% 
+  filter(RPSEGPLAN == LocRef$RPSEGPLAN) %>% 
+  select(Localidade)
+
+PopulacaoSEGPLAN <- PopulacaoProjecao %>% 
+  filter(Ano == AnoRef & RPSEGPLAN == LocRef$RPSEGPLAN) %>% 
+  summarise(sum(Quantidade))
+
+AreaSEGPLAN <- Area %>% 
+  filter(Ano == max(Area$Ano) & RPSEGPLAN == LocRef$RPSEGPLAN) %>% 
+  summarise(sum(AreaTerritorial))
+
+RPSEGPLANPop <- PopulacaoProjecao %>% 
+  filter(RPSEGPLAN == LocRef$RPSEGPLAN) %>% 
+  group_by(Localidade) %>% 
+  summarise(Populacao = sum(Quantidade)) %>% 
+  arrange(desc(Populacao))
