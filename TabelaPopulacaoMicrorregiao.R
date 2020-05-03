cb4adf1120de63817c34a2bdb5bc7deeb6e9ad12
supervedovatto <supervedovatto@hotmail.com>
2020-05-03 20:07:51 -0300

    Tabela da Densidade demográfica nas cidades que compõem a microrregião

---
+++ TabelaPopulacaoMicrorregiao.R
@@ -0,0 +1,19 @@
+Micro.Area <- Area %>%
+  filter(Ano == max(Area$Ano) & Microrregiao == LocRef$Microrregiao) %>% 
+  select(Localidade,AreaTerritorial)
+
+PopulacaoProjecao %>% 
+  filter(Microrregiao == LocRef$Microrregiao) %>% 
+  filter(Ano == AnoRef) %>%
+  group_by(Localidade) %>% 
+  summarise(Populacao = sum(Quantidade)) %>% 
+  arrange(desc(Populacao)) %>%
+  mutate(freq = Populacao/sum(Populacao)) %>% 
+  select(Localidade,Populacao) %>% 
+  merge(Micro.Area,by = "Localidade") %>% 
+  mutate(densidade = round(Populacao/AreaTerritorial,digits = 2)) %>% 
+  kable(col.names = c("Cidade",
+                      "População (hab)",
+                      "Área (km²)",
+                      "Densidade Populacional (hab/km²)"),
+        caption = paste("Densidade Populacional da Microrregião de",LocRef$Microrregiao))
