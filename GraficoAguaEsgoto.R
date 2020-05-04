7c02d2b811f5f4e261bb024fc1d4359a78516ac9
supervedovatto <supervedovatto@hotmail.com>
2020-05-04 14:50:26 -0300

    Mudança de alinhamento

--- GraficoAguaEsgoto.R
+++ GraficoAguaEsgoto.R
@@ -3,7 +3,7 @@ load(file = "Dados/POCV.RData")
 grafico <- AguaEsgoto %>% 
   filter(Localidade == LocRef$Localidade & Ano >= 2005) %>% 
   ggplot(aes(x = Ano, y = Percentual)) + 
-  geom_line(aes(color = Servico), size = 1) +
-  scale_color_brewer(palette="Set1") +
-  theme_minimal() +
-  theme(legend.position="bottom")
+    geom_line(aes(color = Servico), size = 1) +
+    scale_color_brewer(palette="Set1") +
+    theme_minimal() +
+    theme(legend.position="bottom")
