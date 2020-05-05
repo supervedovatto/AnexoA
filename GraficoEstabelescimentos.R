c36d5951ab554266f1c81260f0e985e0e9e03ad3
supervedovatto <supervedovatto@hotmail.com>
2020-05-05 20:49:37 -0300

    Redefinição das labels do eixo x e sua orientação.

--- GraficoEstabelescimentos.R
+++ GraficoEstabelescimentos.R
@@ -1,10 +1,14 @@
 load(file = "Dados/POCV.RData")
 
-grafico <- EstabelecimentosESalas %>% 
+dados <- EstabelecimentosESalas %>% 
   filter(Localidade == LocRef$Localidade) %>%
   select(-starts_with("Salas"),-starts_with("Localidade")) %>% 
+  filter(!is.na(Estabelecimentos))
+
+grafico <- dados %>% 
     ggplot(aes(x = Ano, y = Estabelecimentos)) + 
     geom_line(aes(color = Rede), size = 1) +
     scale_color_brewer(palette="Set1") +
+    scale_x_continuous(breaks = unique(dados$Ano)) +
     theme_minimal() +
-    theme(legend.position="bottom")
+    theme(legend.position="bottom",axis.text.x = element_text(angle = 90))
