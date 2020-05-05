967f0a9ec59d9d03db8bbc3c1480403811759e18
supervedovatto <supervedovatto@hotmail.com>
2020-05-05 20:49:15 -0300

    Redefinição das labels do eixo x e sua orientação.

--- GraficoDocentesHabitantes.R
+++ GraficoDocentesHabitantes.R
@@ -1,15 +1,18 @@
 load(file = "Dados/POCV.RData")
 
-grafico <- PopulacaoProjecao %>% 
+dados <- PopulacaoProjecao %>% 
   group_by(Localidade,Ano) %>% 
   summarise(Populacao =  sum(Quantidade)) %>% 
   merge(Docentes,by = c("Localidade","Ano")) %>% 
   group_by(Localidade,Ano,Rede) %>%  
   summarise(DocHab =  Quantidade/Populacao*100000) %>% 
-  filter(Localidade == LocRef$Localidade) %>% 
+  filter(Localidade == LocRef$Localidade)
+
+grafico <- dados %>% 
   ggplot(aes(x = Ano, y = DocHab)) + 
     geom_line(aes(color = Rede), size = 1) +
     scale_color_brewer(palette="Set1") +
-    labs(y = "Docentes para cada 100 mil habitantes",x="Faixa etária") +
+    labs(y = "Docentes para cada 100 mil habitantes",x="Ano") +
+    scale_x_continuous(breaks = unique(dados$Ano)) +
     theme_minimal() +
-    theme(legend.position="bottom")
+    theme(legend.position="bottom",axis.text.x = element_text(angle = 90))
