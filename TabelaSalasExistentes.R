0af5f92c408bd2aea9866014f675ef9d33920dbf
supervedovatto <supervedovatto@hotmail.com>
2020-05-05 20:53:45 -0300

    Tabela da quantidade real de salas disponíveis nas diferentes redes de ensino.

---
+++ TabelaSalasExistentes.R
@@ -0,0 +1,12 @@
+load(file = "Dados/POCV.RData")
+
+tabela <- EstabelecimentosESalas %>% 
+  merge(RegioesGoias,by = "Localidade") %>% 
+  filter(Microrregiao == LocRef$Microrregiao & Ano == max(EstabelecimentosESalas$Ano)) %>%
+  mutate(TaxaUso = 100*`Salas Utilizadas`/`Salas Existentes`) %>% 
+  select(Localidade,Rede,TaxaUso) %>%
+  filter(!is.na(TaxaUso)) %>% 
+  reshape2::dcast(Localidade ~ Rede,sum,value.var="TaxaUso") %>% 
+  format(big.mark=".",decimal.mark=",",scientific=FALSE,digits = 3) %>%
+  kable(col.names = c("Cidade","Federal (%)","Estadual (%)","Municipal (%)","Particular (%)"),
+        caption = paste("Percentual de utilização das salas de aula nas cidades da Microrregião de",LocRef$Microrregiao,"em",max(EstabelecimentosESalas$Ano),"nas diferentes redes de ensino"))
