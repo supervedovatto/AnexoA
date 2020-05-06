e151a33f5a81956c4f9be29580e94e0149c1a0f5
supervedovatto <supervedovatto@hotmail.com>
2020-05-06 18:44:10 -0300

    A tabela passa a ser relativa à região de planejamento de referência

--- TabelaSalasExistentes.R
+++ TabelaSalasExistentes.R
@@ -1,7 +1,7 @@
 load(file = "Dados/POCV.RData")
 
 tabela <- EstabelecimentosESalas %>% 
-  filter(Microrregiao == LocRef$Microrregiao & Ano == max(EstabelecimentosESalas$Ano)) %>%
+  filter(RPSEGPLAN == LocRef$RPSEGPLAN & Ano == max(EstabelecimentosESalas$Ano)) %>%
   select(Localidade,Rede,`Salas Existentes`) %>%
   filter(!is.na(`Salas Existentes`)) %>% 
   reshape2::dcast(Localidade ~ Rede,sum,value.var="Salas Existentes") %>% 
@@ -9,4 +9,4 @@ tabela <- EstabelecimentosESalas %>%
   adorn_totals() %>% 
   format(big.mark=".",decimal.mark=",",scientific=FALSE) %>%
   kable(col.names = c("Município","Federal","Estadual","Municipal","Particular","Total"),
-        caption = paste("Salas de aulas disponíveis nos municípios da Microrregião de",LocRef$Microrregiao,"em",max(EstabelecimentosESalas$Ano)))
+        caption = paste("Salas de aulas disponíveis nos municípios na Região de Planejamento de",LocRef$RPSEGPLAN,"em",max(EstabelecimentosESalas$Ano)))
