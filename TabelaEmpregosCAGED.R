275f1e88c1390898d3933520f0b5e6aa8c9a35fb
supervedovatto <supervedovatto@hotmail.com>
2020-05-06 18:43:33 -0300

    Mudança no nome do arquivo

--- TabelaEmpregosCAGED.R
+++ TabelaEmpregosCAGED.R
@@ -4,5 +4,4 @@ tabela <- EmpregoCAGED %>%
   filter(!is.na(Quantidade) & RPSEGPLAN == LocRef$RPSEGPLAN & Ano >= max(EmpregoCAGED$Ano)-5) %>% 
   reshape2::dcast(formula = Setor~Situacao,value.var = "Quantidade",fun.aggregate = sum) %>% 
   mutate(Saldo = Admitidos - Desligados) %>% 
-  adorn_totals() %>% 
-  kable(caption = paste("Saldo da geração de empregos por setor entre",max(EmpregoCAGED$Ano)-5,"e",max(EmpregoCAGED$Ano),"na Região de Planejamento",LocRef$RPSEGPLAN,"segundo a CAGED"))
+  kable(caption = paste("Saldo acumulado da geração de empregos por setor entre",max(EmpregoCAGED$Ano)-5,"e",max(EmpregoCAGED$Ano),"na Região de Planejamento",LocRef$RPSEGPLAN,"segundo a CAGED"))
