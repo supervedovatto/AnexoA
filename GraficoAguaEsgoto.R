181433aad3d1a13ea54ee4410b86daf7d63fd257
supervedovatto <supervedovatto@hotmail.com>
2020-05-05 21:47:20 -0300

    Mudança nas labels dos eixos x e y

--- GraficoAguaEsgoto.R
+++ GraficoAguaEsgoto.R
@@ -1,9 +1,14 @@
 load(file = "Dados/POCV.RData")
 
-grafico <- AguaEsgoto %>% 
+dados <- AguaEsgoto %>% 
   filter(Localidade == LocRef$Localidade & Ano >= 2005) %>% 
+  transmute(Ano,Percentual = Percentual/100,Servico)
+
+grafico <- dados %>% 
   ggplot(aes(x = Ano, y = Percentual)) + 
     geom_line(aes(color = Servico), size = 1) +
     scale_color_brewer(palette="Set1") +
+    scale_x_continuous(breaks = unique(dados$Ano)) +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 2)) +
     theme_minimal() +
-    theme(legend.position="bottom")
+    theme(legend.position="bottom",axis.text.x = element_text(angle = 90))
