d4ee362e1d54a17d011cea03415b844252642dd1
supervedovatto <supervedovatto@hotmail.com>
2020-05-15 14:42:17 -0300

    Gráfico da Taxa de aumento do número de vagas em relação ao ano anterior cruzando os dados da RAIS e CAGED.`

---
+++ GraficoSaldoRelativoPorVagasCAGEDxRAIS.R
@@ -0,0 +1,50 @@
+load(file = "Dados/POCV.RData")
+
+level1 <- paste("Município de",LocRef$Localidade)
+tabela1 <- Emprego %>% 
+  mutate(Saldo = Admitidos - Desligados,Taxa = Saldo/Empregos) %>% 
+  filter(!is.na(Taxa) & Empregos>0 & Localidade == LocRef$Localidade & Ano >= 2011) %>% 
+  select(Localidade,Ano, Setor,Taxa) %>% 
+  mutate(Localidade = level1)
+
+level2 <- paste("Região de Planejamento",LocRef$RPSEGPLAN)
+tabela2 <- Emprego %>% 
+  mutate(Saldo = Admitidos - Desligados) %>% 
+  filter(!is.na(Saldo) & Empregos>0 & RPSEGPLAN == LocRef$RPSEGPLAN & Ano >= 2011) %>% 
+  group_by(Setor,Ano) %>% 
+  summarize(Saldo = sum(Saldo,na.rm = TRUE),Empregos = sum(Empregos,na.rm = TRUE)) %>% 
+  mutate(Taxa = Saldo/Empregos,Referencia = level2) %>% 
+  select(Referencia,Ano, Setor,Taxa)
+
+level3 <- paste("Estado de Goiás")
+tabela3 <- Emprego %>% 
+  mutate(Saldo = Admitidos - Desligados) %>% 
+  filter(!is.na(Saldo) & Empregos>0 & Ano >= 2011) %>% 
+  group_by(Setor,Ano) %>% 
+  summarize(Saldo = sum(Saldo,na.rm = TRUE),Empregos = sum(Empregos,na.rm = TRUE)) %>% 
+  mutate(Taxa = Saldo/Empregos,Referencia = level3) %>% 
+  select(Referencia,Ano, Setor,Taxa)
+
+colnames(tabela1) <- colnames(tabela2) <- colnames(tabela3)
+
+dados <- merge(tabela2,tabela1,all = TRUE) %>% 
+  merge(tabela3,all = TRUE)
+
+dados$Referencia <- factor(dados$Referencia,ordered = T,levels = c(level1,level2,level3))
+
+# As taxas da Administração Pública Direta e Indireta são muito baixas devido à estabilidade do serviço público
+# dados <- dados %>%  filter(Setor != "Administração Pública Direta e Indireta")
+
+grafico <- dados %>% 
+  ggplot(aes(x = Ano, y = Taxa)) +
+  geom_line(aes(color = Referencia), stat="identity", size = 1) +
+  scale_color_brewer(palette="Set1") +
+  theme_bw() +
+  theme(legend.position="bottom",
+        axis.text.x = element_text(angle = 90),
+        strip.text = element_text(size = 6),
+        legend.title = element_blank(),
+        legend.direction = "vertical") +
+  scale_x_continuous(breaks = unique(dados$Ano)) +
+  labs(y = "Taxa de Aumento de Postos de Trabalho", x=NULL) +
+  facet_wrap(~Setor, ncol = 2,scales = "free_y")
