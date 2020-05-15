da8c819c460610622e5b263ece442306fa560199
supervedovatto <supervedovatto@hotmail.com>
2020-05-15 14:40:42 -0300

    Mudança na disposição das legendas e das escalas dos eixos y

--- GraficoRendimentoMedioRAIS.R
+++ GraficoRendimentoMedioRAIS.R
@@ -1,20 +1,23 @@
 load(file = "Dados/POCV.RData")
 
+level1 <- paste("Município de",LocRef$Localidade)
 tabela1 <- Emprego %>% 
   filter(!is.na(`Rendimento Médio`) & Localidade == LocRef$Localidade & Ano >= max(EmpregoRAIS$Ano)-10) %>% 
-  select(Localidade,Ano, Setor,`Rendimento Médio`) %>% 
-  data.table()
+  select(Localidade,Ano, Setor,`Rendimento Médio`) %>%  
+  mutate(Localidade = level1)
 
+level2 <- paste("Região de Planejamento",LocRef$RPSEGPLAN)
 tabela2 <- Emprego %>% 
   filter(!is.na(`Rendimento Médio`) & !is.na(Empregos) & RPSEGPLAN == LocRef$RPSEGPLAN & Ano >= max(EmpregoRAIS$Ano)-10) %>% 
   select(RPSEGPLAN,Ano, Setor,`Rendimento Médio`,Empregos) %>% 
   mutate(RendaTotal=`Rendimento Médio`*Empregos) %>% 
   select(RPSEGPLAN,Ano, Setor,Empregos,RendaTotal) %>% 
-  group_by(RPSEGPLAN,Ano,Setor) %>% 
+  group_by(Ano,Setor) %>% 
   summarise(Empregos = sum(Empregos), RendaTotal = sum(RendaTotal)) %>% 
-  mutate(RendimentoMedio = RendaTotal/Empregos) %>% 
-  select(RPSEGPLAN,Ano, Setor,RendimentoMedio)
+  mutate(RendimentoMedio = RendaTotal/Empregos,Referencia = level2) %>% 
+  select(Referencia,Ano, Setor,RendimentoMedio)
 
+level3 <- paste("Estado de Goiás")
 tabela3 <- Emprego %>% 
   filter(!is.na(`Rendimento Médio`) & !is.na(Empregos) & Ano >= max(EmpregoRAIS$Ano)-10) %>% 
   select(Ano, Setor,`Rendimento Médio`,Empregos) %>% 
@@ -24,7 +27,7 @@ tabela3 <- Emprego %>%
   summarise(Empregos = sum(Empregos), RendaTotal = sum(RendaTotal)) %>% 
   mutate(RendimentoMedio = RendaTotal/Empregos) %>% 
   select(Ano,Setor,RendimentoMedio) %>% 
-  mutate(Referencia = "Goiás") %>% 
+  mutate(Referencia = level3) %>% 
   select(Referencia,Ano, Setor,RendimentoMedio)
 
 colnames(tabela1) <- colnames(tabela2) <- colnames(tabela3)
@@ -32,16 +35,18 @@ colnames(tabela1) <- colnames(tabela2) <- colnames(tabela3)
 dados <- merge(tabela2,tabela1,all = TRUE) %>% 
   merge(tabela3,all = TRUE)
 
-dados$Referencia <- factor(dados$Referencia,ordered = T,levels = c("Goiânia","Metropolitana de Goiânia","Goiás"))
-levels(dados$Referencia)
+dados$Referencia <- factor(dados$Referencia,ordered = T,levels = c(level1,level2,level3))
 
 grafico <- dados %>% 
   ggplot(aes(x = Ano, y = RendimentoMedio)) +
-  geom_line(aes(color = Referencia), stat="identity") +
-  scale_color_brewer(palette="Set2") +
+  geom_line(aes(color = Referencia), stat="identity",size = 1) +
+  scale_color_brewer(palette="Set1") +
   theme_bw() +
-  theme(legend.position="bottom", axis.text.x = element_text(angle = 90), strip.text = element_text(size = 6)) +
+  theme(legend.position="bottom",
+        axis.text.x = element_text(angle = 90),
+        strip.text = element_text(size = 6),
+        legend.title = element_blank(),
+        legend.direction = "vertical") +
   scale_x_continuous(breaks = unique(dados$Ano)) +
-  labs(y = "Rendimento Médio", x="Ano") +
-  facet_wrap(~Setor, ncol = 2)
-grafico
+  labs(y = "Rendimento Médio", x=NULL) +
+  facet_wrap(~Setor, ncol = 2,scales = "free_y")
