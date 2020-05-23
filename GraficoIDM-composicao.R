84e5d1bc35383da344e8a0964a4bdd88ece1191a
supervedovatto <supervedovatto@hotmail.com>
2020-05-23 14:14:23 -0300

    Gráficos do IDM

---
+++ GraficoIDM-composicao.R
@@ -0,0 +1,26 @@
+ComposicaoIDM <- function(Eixo){
+  
+  load(file = "Dados/POCV.RData")
+  
+  Municipio <- IDM %>% 
+    filter(!is.na(Valor) & Localidade == LocRef$Localidade & IDM == Eixo)
+  
+  #Número de eixos de avaliação em cada ano
+  eixos <- Municipio %>% 
+    group_by(Ano) %>% 
+    count()
+  
+  Municipio %>% 
+    select(Ano, Variável, Valor) %>% 
+    merge(eixos) %>% 
+    ggplot(aes(x = Ano, y = Valor/n)) +
+    geom_bar(aes(fill = Variável), stat="identity") +
+    scale_fill_manual(values = mypallete) +
+    theme_bw() +
+    theme(legend.position="bottom",
+          strip.text = element_text(size = 6),
+          legend.title = element_blank(),
+          legend.direction = "vertical") +
+    labs(y = paste("IDM",Eixo), x=NULL)
+
+  }
---
+++ GraficoIDM-distribuição.R
@@ -0,0 +1,27 @@
+load(file = "Dados/POCV.RData")
+
+tabela1 <- IDM %>% 
+  merge(RegioesGoias) %>% 
+  filter(!is.na(Valor) & RPSEGPLAN == LocRef$RPSEGPLAN & Ano == max(Ano)) %>% 
+  group_by(Localidade,IDM) %>% 
+  summarise(Valor = mean(Valor,rm.na = TRUE)) %>% 
+  mutate(Referencia = paste("RP",LocRef$RPSEGPLAN)) %>% 
+  select(Referencia, Localidade, IDM, Valor)
+
+tabela2 <- IDM %>% 
+  filter(!is.na(Valor) & Ano == max(Ano)) %>% 
+  group_by(Localidade,IDM) %>% 
+  summarise(Valor = mean(Valor,rm.na = TRUE)) %>% 
+  mutate(Referencia = "Estado de Goiás") %>% 
+  select(Referencia, Localidade, IDM, Valor)
+
+grafico <- rbind(tabela1,tabela2) %>% 
+  ggplot(aes(x=Valor, fill=Referencia)) +
+    geom_density(alpha=0.5)+
+    # geom_vline(data=medias, aes(xintercept=IDEBmedio, fill=Anos),linetype="dashed") +
+    # geom_text(data=medias, aes(x=IDEBmedio, label=paste("Média:",format(IDEBmedio,decimal.mark = ",")), y=1.2),angle=90, vjust = 1.2, text=element_text(size=8))+
+    scale_fill_manual(values = mypallete) +
+    theme_bw() +
+    theme(legend.position="bottom",legend.title = element_blank()) +
+    labs(y = "Densidade", x = "IDM") +
+    facet_wrap(~IDM, ncol = 2)
