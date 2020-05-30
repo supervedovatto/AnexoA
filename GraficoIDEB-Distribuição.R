89635a18d6a56073952ba43462eaad886d59fca8
supervedovatto <supervedovatto@hotmail.com>
2020-05-30 20:41:57 -0300

    Mudança nos nomes dos arquivos.

--- GraficoIDEB-Distribuição.R
+++ GraficoIDEB-Distribuição.R
@@ -14,8 +14,9 @@ grafico <- rbind(tabela1,tabela2) %>%
   ggplot(aes(x=Valor, fill=Anos)) +
     geom_density(alpha=0.5)+
     scale_fill_manual(values = mypallete) +
-    theme_minimal() +
+    theme_bw() +
     theme(legend.position="bottom",legend.title = element_blank())+
     labs(y = "Densidade", x = "IDEB",
-         caption = "Fonte: Elaborado pelo núcleo de base do OMT/GYN a partir de dados do BDE/IMB, com acesso em 19/03/2020.") +
+         caption = "Fonte: Elaborado pelo núcleo de base do OMT/GYN
+         a partir de dados do BDE/IMB, com acesso em 19/03/2020.") +
     facet_grid(vars(Rede),vars(Referencia))
