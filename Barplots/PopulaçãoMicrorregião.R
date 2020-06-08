eba1d7a3168ff15221e6e7456e29149a11b30e22
supervedovatto <supervedovatto@hotmail.com>
2020-06-07 21:43:43 -0300

    O grafico de rosca passa a ser um gráfico de barras.

--- Barplots/PopulaçãoMicrorregião.R
+++ Barplots/PopulaçãoMicrorregião.R
@@ -4,25 +4,24 @@ BarplotsPopulacaoMicrorregiao <- PopulacaoProjecao %>%
            Ano == DataRef & !is.na(Quantidade)) %>%
   group_by(Microrregiao) %>%
   summarise(Habitantes = sum(Quantidade)) %>%
-  arrange(desc(Microrregiao)) %>%
-  mutate(freq = Habitantes / sum(Habitantes)) %>%
-  mutate(ypos = cumsum(freq) - freq / 2) %>%
-  ggplot(aes(x = 2, y = freq, fill = Microrregiao)) +
+  ungroup() %>% 
+  mutate(freq = Habitantes/sum(Habitantes)) %>% 
+  ggplot(aes(x = Microrregiao, y = Habitantes, fill = Microrregiao)) +
   geom_bar(stat = "identity") +
-  geom_text(aes(
-    y = ypos,
-    label = scales::percent(freq, decimal.mark = ",")
-  ),
-  size = 4,
-  color = "white") +
   scale_fill_manual(values = mypallete) +
-  theme_void() +
-  theme(title = element_text(size = 8),
+  geom_text(aes(
+    label = scales::percent(freq, decimal.mark = ","), vjust=-0.5, size=2.5
+  ),
+  size = 3) +
+  theme_bw() +
+  theme(title = element_text(size=8),
+        axis.text = element_text(size=5),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
         axis.title = element_blank()) +
-  xlim(0.5, 2.5) +
   labs(
     x = "",
     y = "",
-    title = "Percentual da população de cada Microrregião",
+    title = paste("População Projetada por Microrregião (",AnoRef,")",sep=""),
     caption = "Fonte: Elaborado pelo OMT/GYN com dados do BDE/IMB. Acesso: 19/03/2020."
   )
