8cb0b6682cc3a2834ddb951c25a68bbf3c17b4b0
supervedovatto <supervedovatto@hotmail.com>
2020-06-09 15:41:06 -0300

    Mudança no nome do arquivo

--- Barplots/PopulacaoMicrorregiao.R
+++ Barplots/PopulacaoMicrorregiao.R
@@ -3,25 +3,26 @@ BarplotsPopulacaoMicrorregiao <- PopulacaoProjecao %>%
   filter(Mesorregiao == LocRef$Mesorregiao &
            Ano == DataRef & !is.na(Quantidade)) %>%
   group_by(Microrregiao) %>%
-  summarise(Habitantes = sum(Quantidade)) %>%
+  summarise(Habitantes = sum(Quantidade)/1000) %>%
   ungroup() %>% 
   mutate(freq = Habitantes/sum(Habitantes)) %>% 
   ggplot(aes(x = Microrregiao, y = Habitantes, fill = Microrregiao)) +
   geom_bar(stat = "identity") +
   scale_fill_manual(values = mypallete) +
   geom_text(aes(
-    label = scales::percent(freq, decimal.mark = ","), vjust=-0.5, size=2.5
+    label = scales::percent(freq, decimal.mark = ","), vjust=-0.1, size=2.5
   ),
   size = 3) +
   theme_bw() +
-  theme(title = element_text(size=8),
-        axis.text = element_text(size=5),
+  theme(title = element_text(size=5),
+        axis.text = element_text(size=10),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         axis.title = element_blank()) +
   labs(
     x = "",
     y = "",
-    title = paste("População Projetada por Microrregião (",AnoRef,")",sep=""),
+    title = paste("População Projetada, em milhares, por Microrregião (",AnoRef,")",sep=""),
     caption = "Fonte: Elaborado pelo OMT/GYN com dados do BDE/IMB. Acesso: 19/03/2020."
   )
+BarplotsPopulacaoMicrorregiao
